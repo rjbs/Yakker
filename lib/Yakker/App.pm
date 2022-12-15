1b800bca641bc64ed1353c15d7360f209bf37a87
Ricardo Signes <rjbs@semiotic.systems>
2022-12-14 20:45:02 -0500

    Yakker::App: move more boilerplate into App, add ->run

--- lib/Yakker/App.pm
+++ lib/Yakker/App.pm
@@ -5,6 +5,41 @@ use Moo::Role;
 use experimental qw(postderef signatures);
 
 requires 'name';
+requires 'activity_class';
+
+sub run ($invocant, $arg = undef) {
+  my $self;
+
+  if (ref $invocant) {
+    if ($arg) {
+      Carp::confess("can't pass arguments to ->run on a Yakker::App");
+    }
+
+    $self = $invocant;
+  } else {
+    $self = $invocant->new($arg // {});
+  }
+
+  unless ($self->activity_class('boot')) {
+    die "can't run app, no boot activity defined";
+  }
+
+  require Yakker::Util;
+  Yakker::Util::activityloop($self->activity('boot'));
+}
+
+sub activity ($self, $name, $arg = {}) {
+  my $class = $self->activity_class($name);
+
+  unless ($class) {
+    die "unknown activity $name";
+  }
+
+  return $class->new({
+    %$arg,
+    app => $self,
+  });
+}
 
 no Moo::Role;
 1;
